d20d1fabe7be9b08c26a6020c0e7c269a8c38c52
Kazuma <sample@gmail.com>
2021-02-20 20:19:39 +0900

    solved isPalindrome

---
+++ easy/palindromenumber/isPalindrome.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func isPalindrome(x int) bool {
+	slice := strings.Split(strconv.Itoa(x), "")
+	for i := 0; i < len(slice); i++ {
+		if slice[i] != slice[len(slice)-i-1] {
+			fmt.Println(slice[i])
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome(1234321))
+}
+
+// If you write like this, the data type can be int32.
+// for _, char := range strconv.Itoa(x) {
+// 	fmt.Println(char)
+// }
+//
+// result
+// 49
+// 50
+// 51
+// 52
+// 51
+// 50
+// 49
+// true
