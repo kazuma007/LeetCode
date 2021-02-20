a6619120f35d8db58a2d8361e5b44ac4b4d07c3d
Kazuma Morishita <28896927+kazuma007@users.noreply.github.com>
2021-02-20 10:14:09 +0900

    Solved Two Sum

---
+++ TwoSum.go
@@ -0,0 +1,13 @@
+// Two Sum
+// https://leetcode.com/submissions/detail/458169746/
+
+func twoSum(nums []int, target int) []int {
+    for i := range nums {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[j] == target - nums[i] {
+                return [] int{i, j}
+            }
+        }
+    }
+    return [] int{}
+}
