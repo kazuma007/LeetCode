5af9217a90f4869e52f19a6b5497f22f131cc8d7
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-05-16 07:49:34 +0900

    added a link to leetcode

--- c/easy/maxSubarray/maximumSubarray.c
+++ c/easy/maxSubarray/maximumSubarray.c
@@ -1,8 +1,9 @@
+ // https://leetcode.com/submissions/detail/490621114/
+
 int maxSubArray(int* nums, int numsSize){
     if(sizeof nums == 1) {
         return nums[0];
     }
-    
     for(int i = 1; i < numsSize; i++) {
         if(nums[i-1] > 0) {
             nums[i] += nums[i-1];
