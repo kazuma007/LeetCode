5c6e4cf231926041d5cfd1c1e7c7a09bf7cbb3b0
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-05-30 15:30:04 +0900

    solved findTheDifference

---
+++ go/easy/findthedifference/find-the-difference.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func findTheDifference(s string, t string) byte {
+	m := make(map[int32]int)
+
+	for _, i := range s {
+		m[i]++
+	}
+	for _, j := range t {
+		m[j]--
+		if m[j] < 0 {
+			return byte(j)
+		}
+	}
+
+	return byte('0')
+}
+
+func main() {
+	s := "abcd"
+	t := "abcde"
+	fmt.Println(findTheDifference(s, t))
+}
